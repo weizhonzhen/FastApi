6f9fad7b18c073a7de026ab9d10e3f6863f4a5f2
weizhonzhen <czdswzz@126.com>
2019-08-31 08:03:04 +0800

    Add files via upload

---
+++ Fast.Api.Web/Startup.cs
@@ -0,0 +1,37 @@
+﻿using FastData.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+
+namespace Fast.Api.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //注册gbk
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding encoding = Encoding.GetEncoding("GB2312");
+
+            //压缩
+            services.AddResponseCompression();
+
+            //注入
+            services.AddTransient<IFastApi, FastApi>();
+            
+            //跨域
+            services.AddCors(options =>
+            {
+                options.AddPolicy("any", builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
+            });
+
+           FastMap.InstanceMap();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMiddleware<FastApiHandler>();
+        }
+    }
+}
