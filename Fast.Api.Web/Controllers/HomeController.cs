76b63678096dfa9a8937077056681f2ae210e3be
weizhonzhen <czdswzz@126.com>
2019-10-06 11:48:31 +0800

    Update HomeController.cs

--- Fast.Api.Web/Controllers/HomeController.cs
+++ Fast.Api.Web/Controllers/HomeController.cs
@@ -1,10 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FastApiGatewayDb.Ui.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("help")]
         public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("xml")]
+        public IActionResult Xml()
         {
             return View();
         }
