c9f961ab5c55de402b9b195ec0df1d172081adf1
weizhonzhen <czdswzz@126.com>
2019-10-26 23:09:35 +0800

    Add files via upload

---
+++ Fast.Api.Mvc/Startup.cs
@@ -0,0 +1,42 @@
+﻿using FastData.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+
+namespace Fast.Api.Mvc
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding encoding = Encoding.GetEncoding("GB2312");
+
+            services.AddResponseCompression();
+
+            services.AddTransient<IFastApi, FastApi>();
+            
+            services.AddCors(options =>
+            {
+                options.AddPolicy("any", builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
+            });
+
+            services.AddMvc();
+
+            FastMap.InstanceMap();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStaticFiles();
+            app.UseMiddleware<FastApiHandler>();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{action=index}/{id?}");
+            });
+        }
+    }
+}
