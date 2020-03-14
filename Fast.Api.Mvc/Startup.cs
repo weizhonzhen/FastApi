041e7b41e1a29a6c73b04e8c5aa6b3346094cbbb
weizhonzhen <czdswzz@126.com>
2020-03-14 09:58:10 +0800

    Update Startup.cs

--- Fast.Api.Mvc/Startup.cs
+++ Fast.Api.Mvc/Startup.cs
@@ -1,6 +1,9 @@
 using FastData.Core;
+using FastUntility.Core.Base;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Text;
 using System.Text.Encodings.Web;
@@ -32,6 +35,21 @@ namespace Fast.Api.Mvc
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseExceptionHandler(error =>
+            {
+                error.Use(async (context, next) =>
+                {
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        BaseLog.SaveLog(contextFeature.Error.Message, "error");
+                        context.Response.ContentType = "application/json";
+                        context.Response.StatusCode = 404;
+                        await context.Response.WriteAsync(contextFeature.Error.Message);
+                    }
+                });
+            });
+
             app.UseStaticFiles();
             app.UseMiddleware<FastApiHandler>();
             app.UseMvc(routes =>
