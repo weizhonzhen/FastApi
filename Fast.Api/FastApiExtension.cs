7ed0601777cd8f625a5873d249909048ab3ce7f5
weizhonzhen <czdswzz@126.com>
2020-11-19 07:55:19 +0800

    Add files via upload

---
+++ Fast.Api/FastApiExtension.cs
@@ -0,0 +1,15 @@
+﻿using Fast.Api;
+using FastUntility.Core;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class FastApiExtension
+    {
+        public static IServiceCollection AddFastApi(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IFastApi, FastApi>();
+            ServiceContext.Init(new ServiceEngine(serviceCollection.BuildServiceProvider()));
+            return serviceCollection;
+        }
+    }
+}
