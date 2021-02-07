6d425bc31975e693e379455f0f40b90abd4d030f
weizhonzhen <czdswzz@126.com>
2021-02-07 08:51:52 +0800

    Add files via upload

--- Fast.Api/FastApiExtension.cs
+++ Fast.Api/FastApiExtension.cs
@@ -29,4 +29,15 @@ namespace Microsoft.Extensions.DependencyInjection
             return app.UseMiddleware<FastApiHandler>(options);
         }
     }
+
+    public class ConfigModel
+    {
+        public bool IsResource { get; set; }
+
+        public string dbKey { get; set; }
+
+        public string dbFile { get; set; } = "db.json";
+
+        public string mapFile { get; set; } = "map.json";
+    }
 }
