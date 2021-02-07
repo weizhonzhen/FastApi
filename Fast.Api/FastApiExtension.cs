fd9fca26be83e01a903653988ea88f4a8b575130
weizhonzhen <czdswzz@126.com>
2021-02-07 08:49:30 +0800

    Add files via upload

--- Fast.Api/FastApiExtension.cs
+++ Fast.Api/FastApiExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Fast.Api;
+using FastData.Core;
 using FastData.Core.Repository;
 using FastUntility.Core;
 using Microsoft.AspNetCore.Builder;
@@ -8,8 +9,13 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class FastApiExtension
     {
-        public static IServiceCollection AddFastApi(this IServiceCollection serviceCollection)
+        public static IServiceCollection AddFastApi(this IServiceCollection serviceCollection, ConfigModel config)
         {
+            if (config.IsResource)
+                FastMap.InstanceMapResource(config.dbKey, config.dbFile, config.mapFile);
+            else
+                FastMap.InstanceMap(config.dbKey, config.dbFile, config.mapFile);
+
             serviceCollection.AddTransient<IFastRepository, FastRepository>();
             serviceCollection.AddTransient<IFastApi, FastApi>();
             ServiceContext.Init(new ServiceEngine(serviceCollection.BuildServiceProvider()));
