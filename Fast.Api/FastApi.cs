a047f0064ff537a4795a9f8f5d3dbe495ea4b236
weizhonzhen <czdswzz@126.com>
2021-02-09 23:18:23 +0800

    Add files via upload

--- Fast.Api/FastApi.cs
+++ Fast.Api/FastApi.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using FastData.Core.Repository;
+using FastData.Core.Context;
+using FastData.Core.Base;
 
 namespace Fast.Api
 {
@@ -17,172 +19,165 @@ namespace Fast.Api
         public async Task ContentAsync(HttpContext context, IFastRepository IFast, bool IsResource, string dbFile = "db.json")
         {
             var name = context.Request.Path.Value.ToStr().Substring(1, context.Request.Path.Value.ToStr().Length - 1).ToLower();
-           
-                var urlParam = HttpUtility.UrlDecode(GetUrlParam(context));
-                var success = true;
-                var dic = new Dictionary<string, object>();
 
-                context.Response.StatusCode = 200;
-                context.Response.ContentType = "application/Json";
+            var urlParam = HttpUtility.UrlDecode(GetUrlParam(context));
+            var success = true;
+            var dic = new Dictionary<string, object>();
+            var dbKey = IFast.MapDb(name).ToStr();
 
-                if (!IFast.IsExists(name))
-                {
-                    dic.Add("success", false);
-                    dic.Add("error", "接口不存在");
-                    await context.Response.WriteAsync(BaseJson.ModelToJson(dic), Encoding.UTF8).ConfigureAwait(false);
-                }
-                else if (IFast.MapDb(name).ToStr() == "")
-                {
-                    dic.Add("success", false);
-                    dic.Add("error", string.Format("map id {0}的db没有配置", name));
-                    await context.Response.WriteAsync(BaseJson.ModelToJson(dic), Encoding.UTF8).ConfigureAwait(false);
-                }
-                else if (IFast.IsExists(name))
-                {
-                    var data = new List<Dictionary<string, object>>();
-                    var param = new List<DbParameter>();
+            context.Response.StatusCode = 200;
+            context.Response.ContentType = "application/Json";
 
-                    foreach (var item in IFast.MapParam(name))
+            if (!IFast.IsExists(name))
+            {
+                dic.Add("success", false);
+                dic.Add("error", "接口不存在");
+                await context.Response.WriteAsync(BaseJson.ModelToJson(dic), Encoding.UTF8).ConfigureAwait(false);
+            }
+            else if (dbKey == "")
+            {
+                dic.Add("success", false);
+                dic.Add("error", string.Format("map id {0}的db没有配置", name));
+                await context.Response.WriteAsync(BaseJson.ModelToJson(dic), Encoding.UTF8).ConfigureAwait(false);
+            }
+            else if (IFast.IsExists(name))
+            {
+                var param = new List<DbParameter>();
+
+                foreach (var item in IFast.MapParam(name))
+                {
+                    var checkKey = IFast.MapCheckMap(name, item);
+                    var existsKey = IFast.MapExistsMap(name, item);
+                    var tempParam = DbProviderFactories.GetFactory(IFast.MapDb(name), IsResource, dbFile).CreateParameter();
+
+                    tempParam.ParameterName = item;
+                    tempParam.Value = GetUrlParam(urlParam, item);
+                    param.Add(tempParam);
+
+                    if (!string.IsNullOrEmpty(IFast.MapRequired(name, item)))
                     {
-                        var checkKey = IFast.MapCheckMap(name, item);
-                        var existsKey = IFast.MapExistsMap(name, item);
-                        var tempParam = DbProviderFactories.GetFactory(IFast.MapDb(name), IsResource, dbFile).CreateParameter();
-                    
-                        tempParam.ParameterName = item;
-                        tempParam.Value = GetUrlParam(urlParam, item);
-                        param.Add(tempParam);
-
-                        if (!string.IsNullOrEmpty(IFast.MapRequired(name, item)))
+                        if (!(IFast.MapRequired(name, item).ToLower() == "true" && !string.IsNullOrEmpty(tempParam.Value.ToStr())))
                         {
-                            if (!(IFast.MapRequired(name, item).ToLower() == "true" && !string.IsNullOrEmpty(tempParam.Value.ToStr())))
-                            {
-                                dic.Add("error", string.Format("{0}不能为空", item));
-                                param.Remove(tempParam);
-                                break;
-                            }
-                        }
-
-                        if (IFast.MapMaxlength(name, item).ToInt(0) != 0)
-                        {
-                            if (!(IFast.MapMaxlength(name, item).ToInt(0) >= tempParam.Value.ToStr().Length))
-                            {
-                                dic.Add("error", string.Format("{0}：{1}，最大长度{2}", item, tempParam.Value.ToStr(), IFast.MapMaxlength(name, item)));
-                                param.Remove(tempParam);
-                                break;
-                            }
-                        }
-
-                        if (!string.IsNullOrEmpty(existsKey))
-                        {
-                            var existsListParam = new List<DbParameter>();
-                            var existsParam = DbProviderFactories.GetFactory(IFast.MapDb(existsKey), IsResource, dbFile).CreateParameter();
-                            existsParam.ParameterName = item;
-                            existsParam.Value = tempParam.Value;
-                            existsListParam.Add(existsParam);
-
-                            var checkData = IFast.Query(existsKey, existsListParam.ToArray())?.First() ?? new Dictionary<string, object>();
-                            if (checkData.GetValue("count").ToStr().ToInt(0) >= 1)
-                            {
-                                dic.Add("error", string.Format("{0}：{1}已存在", item, tempParam.Value));
-                                param.Remove(tempParam);
-                                break;
-                            }
-                        }
-
-                        if (!string.IsNullOrEmpty(checkKey))
-                        {
-                            var checkListParam = new List<DbParameter>();
-                            var checkParam = DbProviderFactories.GetFactory(IFast.MapDb(checkKey), IsResource, dbFile).CreateParameter();
-                            checkParam.ParameterName = item;
-                            checkParam.Value = GetUrlParam(urlParam, item);
-                            checkListParam.Add(checkParam);
-
-                            var checkData = IFast.Query(existsKey, checkListParam.ToArray())?.First() ?? new Dictionary<string, object>();
-                            if (checkData.GetValue("count").ToStr().ToInt(0) < 1)
-                            {
-                                dic.Add("error", string.Format("{0}：{1}不存在", item, tempParam.Value));
-                                param.Remove(tempParam);
-                                break;
-                            }
-                        }
-
-                        if (IFast.MapDate(name, item).ToStr().ToLower() == "true")
-                        {
-                            if (!BaseRegular.IsDate(tempParam.Value.ToStr()))
-                            {
-                                dic.Add("error", string.Format("{0}：{1},不是日期类型", item, tempParam.Value));
-                                param.Remove(tempParam);
-                                break;
-                            }
-                            tempParam.Value = tempParam.Value.ToDate();
-                            tempParam.DbType = System.Data.DbType.DateTime;
-                        }
-
-                        if (tempParam.Value.ToStr() == "")
+                            dic.Add("error", string.Format("{0}不能为空", item));
                             param.Remove(tempParam);
-                    }
-
-                    if (IFast.MapType(name).ToStr().ToLower() == AppConfig.PageAll && dic.Count == 0)
-                    {
-                        var pageSize = GetUrlParam(urlParam, "PageSize");
-                        var pageId = GetUrlParam(urlParam, "PageId");
-                        success = true;
-                        var page = new PageModel();
-
-                        page.PageSize = pageSize.ToInt(0) == 0 ? 10 : pageSize.ToInt(0);
-                        page.PageId = pageId.ToInt(0) == 0 ? 1 : pageId.ToInt(0);
-                        var info = IFast.QueryPage(page, name, param.ToArray());
-                        dic.Add("data", info.list);
-                        dic.Add("page", info.pModel);
-                    }
-                    else if (IFast.MapType(name).ToStr().ToLower() == AppConfig.Page && param.Count > 0)
-                    {
-                        var pageSize = GetUrlParam(urlParam, "PageSize");
-                        var pageId = GetUrlParam(urlParam, "PageId");
-                        success = true;
-                        var page = new PageModel();
-
-                        page.PageSize = pageSize.ToInt(0) == 0 ? 10 : pageSize.ToInt(0);
-                        page.PageId = pageId.ToInt(0) == 0 ? 1 : pageId.ToInt(0);
-                        var info = IFast.QueryPage(page, name, param.ToArray());
-                        dic.Add("data", info.list);
-                        dic.Add("page", info.pModel);
-                    }
-                    else if (IFast.MapType(name).ToStr().ToLower() == AppConfig.All && dic.Count == 0)
-                    {
-                        success = true;
-                        data = IFast.Query(name, param.ToArray());
-                        dic.Add("data", data);
-                    }
-                    else if (IFast.MapType(name).ToStr().ToLower() == AppConfig.Write && param.Count > 0)
-                    {
-                        var result = IFast.Write(name, param.ToArray());
-                        if (result.IsSuccess)
-                            success = true;
-                        else
-                        {
-                            success = false;
-                            dic.Add("error", result.Message);
+                            break;
                         }
                     }
+
+                    if (IFast.MapMaxlength(name, item).ToInt(0) != 0)
+                    {
+                        if (!(IFast.MapMaxlength(name, item).ToInt(0) >= tempParam.Value.ToStr().Length))
+                        {
+                            dic.Add("error", string.Format("{0}：{1}，最大长度{2}", item, tempParam.Value.ToStr(), IFast.MapMaxlength(name, item)));
+                            param.Remove(tempParam);
+                            break;
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(existsKey))
+                    {
+                        var existsListParam = new List<DbParameter>();
+                        var existsParam = DbProviderFactories.GetFactory(IFast.MapDb(existsKey), IsResource, dbFile).CreateParameter();
+                        existsParam.ParameterName = item;
+                        existsParam.Value = tempParam.Value;
+                        existsListParam.Add(existsParam);
+
+                        var checkData = IFast.Query(existsKey, existsListParam.ToArray())?.First() ?? new Dictionary<string, object>();
+                        if (checkData.GetValue("count").ToStr().ToInt(0) >= 1)
+                        {
+                            dic.Add("error", string.Format("{0}：{1}已存在", item, tempParam.Value));
+                            param.Remove(tempParam);
+                            break;
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(checkKey))
+                    {
+                        var checkListParam = new List<DbParameter>();
+                        var checkParam = DbProviderFactories.GetFactory(IFast.MapDb(checkKey), IsResource, dbFile).CreateParameter();
+                        checkParam.ParameterName = item;
+                        checkParam.Value = GetUrlParam(urlParam, item);
+                        checkListParam.Add(checkParam);
+
+                        var checkData = IFast.Query(existsKey, checkListParam.ToArray())?.First() ?? new Dictionary<string, object>();
+                        if (checkData.GetValue("count").ToStr().ToInt(0) < 1)
+                        {
+                            dic.Add("error", string.Format("{0}：{1}不存在", item, tempParam.Value));
+                            param.Remove(tempParam);
+                            break;
+                        }
+                    }
+
+                    if (IFast.MapDate(name, item).ToStr().ToLower() == "true")
+                    {
+                        if (!BaseRegular.IsDate(tempParam.Value.ToStr()))
+                        {
+                            dic.Add("error", string.Format("{0}：{1},不是日期类型", item, tempParam.Value));
+                            param.Remove(tempParam);
+                            break;
+                        }
+                        tempParam.Value = tempParam.Value.ToDate();
+                        tempParam.DbType = System.Data.DbType.DateTime;
+                    }
+
+                    if (tempParam.Value.ToStr() == "")
+                        param.Remove(tempParam);
+                }
+
+                if (dic.Count > 0)
+                    await context.Response.WriteAsync(BaseJson.ModelToJson(dic), Encoding.UTF8).ConfigureAwait(false);
+                else if (IFast.MapType(name).ToStr().ToLower() == AppConfig.PageAll || IFast.MapType(name).ToStr().ToLower() == AppConfig.Page)
+                {
+                    using (var db = new DataContext(dbKey))
+                    {
+                        success = true;
+                        var pageSize = GetUrlParam(urlParam, "PageSize");
+                        var pageId = GetUrlParam(urlParam, "PageId");
+                        var page = new PageModel();
+
+                        page.PageSize = pageSize.ToInt(0) == 0 ? 10 : pageSize.ToInt(0);
+                        page.PageId = pageId.ToInt(0) == 0 ? 1 : pageId.ToInt(0);
+                        var tempParam = param.ToArray();
+                        var sql = MapXml.GetMapSql(name, ref tempParam, db, dbKey);
+                        var info = db.GetPageSql(page, sql, tempParam).PageResult;
+                        dic.Add("data", info.list);
+                        dic.Add("page", info.pModel);
+                    }
+                }
+                else if (IFast.MapType(name).ToStr().ToLower() == AppConfig.All)
+                {
+                    success = true;
+                    var data = IFast.Query(name, param.ToArray());
+                    dic.Add("data", data);
+                }
+                else if (IFast.MapType(name).ToStr().ToLower() == AppConfig.Write && param.Count > 0)
+                {
+                    var result = IFast.Write(name, param.ToArray());
+                    if (result.IsSuccess)
+                        success = true;
                     else
                     {
-                        if (param.Count > 0)
-                        {
-                            success = true;
-                            data = IFast.Query(name, param.ToArray());
-
-                            dic.Add("data", data);
-                        }
-                        else
-                            success = false;
+                        success = false;
+                        dic.Add("error", result.Message);
                     }
-
-                    dic.Add("success", success);
-                    await context.Response.WriteAsync(BaseJson.ModelToJson(dic), Encoding.UTF8).ConfigureAwait(false);
                 }
+                else
+                {
+                    if (param.Count > 0)
+                    {
+                        success = true;
+                        var data = IFast.Query(name, param.ToArray());
+                        dic.Add("data", data);
+                    }
+                    else
+                        success = false;
+                }
+
+                dic.Add("success", success);
+                await context.Response.WriteAsync(BaseJson.ModelToJson(dic), Encoding.UTF8).ConfigureAwait(false);
+            }
         }
-        
+
         #region 获取参数
         /// <summary>
         /// 获取参数
