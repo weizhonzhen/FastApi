1d0f6e400850dc589c6281f1bd4eb4a0861c046d
weizhonzhen <czdswzz@126.com>
2021-03-03 14:09:31 +0800

    Add files via upload

--- Fast.Api/FastApiExtension.cs
+++ Fast.Api/FastApiExtension.cs
@@ -11,8 +11,11 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class FastApiExtension
     {
-        public static IServiceCollection AddFastApi(this IServiceCollection serviceCollection, ConfigApi config)
+        public static IServiceCollection AddFastApi(this IServiceCollection serviceCollection, Action<ConfigApi> action)
         {
+            var config = new ConfigApi();
+            action(config);
+
             if (string.IsNullOrEmpty(config.dbKey))
                 throw new Exception("config dbkey is not null");
 
