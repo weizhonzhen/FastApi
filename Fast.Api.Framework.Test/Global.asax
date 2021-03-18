1be02bd4f7154da89c4e2fd809b932451a7d16da
weizhonzhen <czdswzz@126.com>
2021-03-18 12:55:34 +0800

    Add files via upload

---
+++ Fast.Api.Framework.Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fast.Api.Framework.Test.MvcApplication" Language="C#" %>
---
+++ Fast.Api.Framework.Test/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Fast.Api.Framework.Test
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            FastData.FastMap.InstanceMap("Test");
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
