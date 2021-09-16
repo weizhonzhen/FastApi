1d30b8162e94d40b43815573bf2b47e21ec1589a
weizhonzhen <czdswzz@126.com>
2021-09-16 16:43:57 +0800

    Add files via upload

--- Fast.Api/FastApiExtension.cs
+++ Fast.Api/FastApiExtension.cs
@@ -19,15 +19,16 @@ namespace Microsoft.Extensions.DependencyInjection
             if (string.IsNullOrEmpty(config.dbKey))
                 throw new Exception("config dbkey is not null");
 
+            serviceCollection.AddTransient<IFastRepository, FastRepository>();
+            serviceCollection.AddTransient<IFastApi, FastApi>();
+            ServiceContext.Init(new ServiceEngine(serviceCollection.BuildServiceProvider()));
+
             var projectName = Assembly.GetCallingAssembly().GetName().Name;
             if (config.IsResource)
                 FastMap.InstanceMapResource(config.dbKey, config.dbFile, config.mapFile, projectName);
             else
                 FastMap.InstanceMap(config.dbKey, config.dbFile, config.mapFile);
 
-            serviceCollection.AddTransient<IFastRepository, FastRepository>();
-            serviceCollection.AddTransient<IFastApi, FastApi>();
-            ServiceContext.Init(new ServiceEngine(serviceCollection.BuildServiceProvider()));
             return serviceCollection;
         }
 
