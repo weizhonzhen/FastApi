07638559deca3c6aa0a0357cb1f49acb75dd26d6
weizhonzhen <czdswzz@126.com>
2022-03-30 17:02:34 +0800

    Add files via upload

---
+++ Fast.Api.Blazor/wwwroot/Scripts/help.js
@@ -0,0 +1,43 @@
+﻿window.methods = {
+    titleClick: function (id) {
+        if ($("#" + id).css("display") == "none")
+            $("#" + id).show();
+        else
+            $("#" + id).hide();
+
+        $("div .title").each(function () {
+            if ($(this).data("id") != id)
+                $("#" + $(this).data("id")).hide();
+        });
+    },
+    paramterClick: function (id) {
+        debugger;
+        $("div input").each(function () {
+            $(this).val("");
+        });
+        $("div textarea").each(function () {
+            $(this).val("");
+        });
+
+        if ($("#" + id).css("display") == "none")
+            $("#" + id).show();
+        else
+            $("#" + id).hide();
+
+        $("table").each(function () {
+            if ($(this).attr("id") != id)
+                $(this).hide();
+        });
+    },
+    submitClick: function (id,result,url) {
+        $.ajax({
+            type: "POST",
+            timeout: 1000 * 60,
+            url: url,
+            data: $("#" + id + " input").serialize(),
+            success: function (data) {
+                $("#" + result).val(data);
+            }
+        });
+    }
+}
