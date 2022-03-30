771a33e752d4a9f8eb143bdd6e050a709a1d4532
weizhonzhen <czdswzz@126.com>
2022-03-30 16:51:11 +0800

    Add files via upload

---
+++ Fast.Api.Blazor/Filter/FastServiceFilter.cs
@@ -0,0 +1,20 @@
+﻿namespace Fast.Api.Blazor.Filter
+{
+    public class FastServiceFilter : FastAopAttribute
+    {
+        public override void After(AfterContext context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Before(BeforeContext context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Exception(ExceptionContext exception)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
