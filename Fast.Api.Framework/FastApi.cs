a81b3c325baef39ec813fc44381ae508e0e1534e
weizhonzhen <czdswzz@126.com>
2022-08-23 15:00:47 +0800

    Add files via upload

---
+++ Fast.Api.Framework/FastApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FastData;
+using FastData.Model;
+
+namespace Fast.Api.Framework
+{
+    public class FastApi
+    {
+        public static void Init(Action<ConfigData> action)
+        {
+            FastMap.Init(action);
+        }
+
+        public static void InitGeneric(Action<ConfigRepository> action)
+        {
+            FastMap.InitGeneric(action);
+        }
+    }
+}
